2f1aedb9d8743fbe3a465f505810960bfb0569ce
Rodrigo dos Santos <rodrigo.contato@live.com>
2021-05-25 12:00:19 -0300

    Date

--- hdcevents/resources/views/events/create.blade.php
+++ hdcevents/resources/views/events/create.blade.php
@@ -16,6 +16,10 @@
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento">
             </div>
             <div class="form-group">
+                <label for="date">Data do evento:</label>
+                <input type="date" class="form-control" id="date" name="date">
+            </div>
+            <div class="form-group">
                 <label for="city">Cidade:</label>
                 <input type="text" class="form-control" id="city" name="city" placeholder="Local do Evento">
             </div>
@@ -29,6 +33,24 @@
             <div class="form-group">
                 <label for="description">Descrição:</label>
                 <textarea name="description" class="form-control" id="description" placeholder="O que vai acontecer no evento?"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="title">Adicione itens de infraestrutura:</label>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cadeiras">Cadeiras
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Palco">Palco
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cerveja grátis">Cerveja grátis
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Open food">Open food
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Brindes">Brindes
+                </div>
             </div>
             <input type="submit" class="btn btn-primary" value="Criar Evento">
         </form>
